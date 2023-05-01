e61c6bed792cd2669fd783cfcda02dfdbc493ed9
Tricia Sykes <triciasykes@Tricias-MacBook-Pro-2.local>
2023-05-01 12:00:45 -0700

    adds seeds and cors

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,31 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+cats = [
+  {
+    name:"Kevin",
+    age:9,
+    enjoys: "talk to the dogs who walk by the window",
+    image: "https://c2.peakpx.com/wallpaper/591/784/17/cat-animal-pet-cat-face-head-black-wallpaper-preview.jpg"
+  },
+  {
+    name: "Mister",
+    age:12,
+    enjoys: "stealing human food from the table",
+    image: "https://www.publicdomainpictures.net/pictures/200000/nahled/ragdoll-cat-with-green-eyes-14766395657Vf.jpg"
+  },
+  {
+    name:"Luna",
+    age:5,
+    enjoys: "eating house plants and breaking stuff",
+    image: 'https://petkeen.com/wp-content/uploads/2021/04/Blue-norwegian-forest-cat_Elisa-Putti_Shutterstock-760x507.jpg'
+  },
+  {
+    name:"Maki",
+    age: 1,
+    enjoys: "talking to himself and singing",
+    image: "https://c2.peakpx.com/wallpaper/296/908/856/cat-big-eyes-female-animal-fur-wallpaper-preview.jpg"
+  },
+]
+
+cats.each do |cat|
+  Cat.create cat
+  puts "creating cat #{cat}"
+end
